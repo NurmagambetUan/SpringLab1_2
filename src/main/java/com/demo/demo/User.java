c6439e0f0b04587fc21c8b94116caabd2fd1cbc1
NurmagambetUlan <nurmagambet.ulan@gmail.com>
2021-02-12 19:34:55 +0600

    Initial commit

---
+++ src/main/java/com/demo/demo/User.java
@@ -0,0 +1,35 @@
+//package com.demo.demo;
+//
+//import javax.persistence.*;
+//@Entity
+//@Table(name = "user_tbl")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Integer id;
+//    private String name;
+//    public User() {
+//    }
+//    public User(String name, int age) {
+//        this.name = name;
+//    }
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
+//}
---
+++ src/main/java/com/demo/demo/UserRepository.java
@@ -0,0 +1,7 @@
+//package com.demo.demo;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface UserRepository extends CrudRepository<User, Integer> {
+//    User findByName(String name);
+//}
